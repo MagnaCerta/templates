3715bd1267d2c5e76862f8325e0446a68a949e9c
Angel Calderaro <calderaro94@gmail.com>
2021-03-31 15:52:38 -0600

    add context

---
+++ context.ts
@@ -0,0 +1,26 @@
+interface CertaTemplateContext {
+  templateName: string;
+  templateType: string;
+  templateDescription: string;
+  templateContext: string;
+  templateCreatedAt: string;
+  organizationId: string;
+  organizationName: string;
+  organizationLegalRepresentativeName: string;
+  organizationCity: string;
+  organizationState: string;
+  organizationCountry: string;
+  digitalPenId: string;
+  digitalPenLegalName: string;
+  digitalPenLegalRepresentativeId: string;
+  certificateSerialNumber: string;
+  certificateCreatedAt: string;
+  certificateExpirationDate: string;
+  certificateValidFrom: string;
+  date: string;
+  issuerId: string;
+  issuerFirstname: string;
+  issuerLastname: string;
+  logo: string;
+  uuid: string;
+}
